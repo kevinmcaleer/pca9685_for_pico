8d8f6184d1b3b3f69a6819c3ea9305cfa3d9a11c
Jim Tannenbaum <jetannenbaum@gmail.com>
2022-09-20 13:50:47 -0400

    Update demo.py
    Removed references to pca8695 library and initialization, since this is done in the servo initialization.

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from pca9685 import PCA9685
+#from pca9685 import PCA9685
 # from picocat import Servos
 from machine import I2C, Pin
 from servo import Servos
@@ -8,7 +8,7 @@ scl = Pin(1)
 id = 0
 i2c = I2C(id=id, sda=sda, scl=scl)
 
-pca = PCA9685(i2c=i2c)
+#pca = PCA9685(i2c=i2c)
 # pca.i2c = i2c
 servo = Servos(i2c=i2c)
 servo.position(index=0, degrees=180)
